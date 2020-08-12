4ab4b61ca83104e2be6c8d9585f822dc9c4ae799
League <League@DESKTOP-A86R9LN>
2020-08-12 15:38:22 -0700

    Intro to exception handleing yay, exception handleing stuff

---
+++ src/_01_introduction/CustomException.java
@@ -0,0 +1,13 @@
+package _01_introduction;
+
+public class CustomException extends Exception {
+
+	public static void main(String[] args) {
+		terminate();
+	}
+
+	public static void terminate() {
+		System.exit(0);
+	}
+
+}
--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,27 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+
+	public double divide(double numerator, double denominator) throws Exception {
+		double quotient;
+		if (denominator == 0) {
+			throw new IllegalArgumentException();
+		} else {
+			quotient = numerator / denominator;
+		}
+		return quotient;
+	}
+
+	public String reverseString(String input) throws Exception {
+		if (input.equals("")) {
+			throw new IllegalStateException();
+		} else {
+			String output = "";
+			for (int i = input.length(); i >= 0; i--) {
+				output = output + input.charAt(i);
+			}
+			return output;
+		}
+	}
+
 }
